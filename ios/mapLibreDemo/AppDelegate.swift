f2e3e271f8387d69fb3e9e5fc3e64859d1fb6c32
Joizel <onepushsing@gmail.com>
2025-10-04 23:21:34 +0800

    map libre added

--- ios/mapLibreDemo/AppDelegate.swift
+++ ios/mapLibreDemo/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "MyMapApp",
+      withModuleName: "mapLibreDemo",
       in: window,
       launchOptions: launchOptions
     )
